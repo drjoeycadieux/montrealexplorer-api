e9e6dd3089404cc7cfdadf460401e76f35677ab4
joeycadieux161 <joeycadieux161@gmail.com>
2025-10-25 19:29:13 +0000

    Backend services.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import os
+from flask import Flask, render_template
 
-from flask import Flask
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.config["NAME"] = os.getenv("NAME", "World")
+    app.config["PORT"] = int(os.getenv("PORT", 3000))
 
-@app.route("/")
-def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+    @app.route("/")
+    def hello_world():
+        name = app.config["NAME"]
+        return render_template("index.html", name=name)
+
+    @app.route("/services")
+    def services():
+        return render_template("services.html")
+
+    @app.route("/login")
+    def login():
+        """Login page for Montreal Explorer backend."""
+        return render_template("login.html")
+
+    return app
+
 
 if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=app.config["PORT"])
